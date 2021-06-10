7fbd26b55a23a3c420b0f56cff7fe6614310dfb6
uzakotim <timauzakov@gmail.com>
2021-06-10 18:19:13 +0500

    Preliminary Blob Detector

---
+++ PreliminaryCode/blob_detector/CMakeFiles/blob_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blob_detector.dir/display_image.cpp.o"
+  "blob_detector"
+  "blob_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blob_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PreliminaryCode/blob_detector/display_image.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+
+int main(int argc, char** argv)
+{
+    cv::Mat image;
+    cv::VideoCapture cap;
+
+    // open the default camera using default API
+    // cap.open(0);
+    // or
+    int deviceID = 0;            // 0 = open default camera
+    int apiID= cv::CAP_ANY;      // 0 = autodetect default API
+
+    cap.open(deviceID,apiID);
+    // check if succeded
+    if (!cap.isOpened())
+    {
+        std::cerr<< "ERROR! Unable to open camera \n";
+        return -1;
+    } 
+
+    std::cout<<"Start grabbing"<<'\n'<<"Press any key to terminate"<<'\n';
+
+    for (;;)
+    {
+        cap.read(image);
+        if (image.empty())          // check if succeded
+        {
+            std::cerr<<"ERROR! blank frame grabbed\n";
+            break;
+        }
+        cv::imshow("Live", image);  // show image
+        if (cv::waitKey(5) >= 0)    // break if a key is pressed
+            break;
+
+    }
+
+    return 0;
+}
