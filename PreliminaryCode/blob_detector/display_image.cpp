fc0f4adac5f050d705596dac0bd9676611728269
uzakotim <timauzakov@gmail.com>
2021-06-11 17:12:03 +0500

    Decorated Blob Detector

--- PreliminaryCode/blob_detector/display_image.cpp
+++ PreliminaryCode/blob_detector/display_image.cpp
@@ -13,12 +13,12 @@ int FindMaxAreaContourId(std::vector<std::vector<cv::Point>> contours)
 
     for (size_t i = 0;i<contours.size();i++)
     {
-        double newArea = cv::contourArea(contours.at(i));
-        if  (newArea > maxArea)
-        {
-                maxArea = newArea;
-                maxAreaContourId = i;
-        }
+            double   newArea = cv::contourArea(contours.at(i));
+            if(newArea > maxArea)
+            {
+                    maxArea = newArea;
+                    maxAreaContourId = i;
+            }
     }
     return maxAreaContourId;
 }
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
     
     cv::Point2f                         last_recognized_center_point{(0.f,0.f)};
 
-    //Kalman filter parameters
+    //---Kalman Filter Parameters---->>----
     cv::KalmanFilter KF(4,2,0);
     KF.transitionMatrix = (cv::Mat_<float>(4,4) << 1,0,1,0, 0,1,0,1, 0,0,1,0, 0,0,0,1);
     cv::Mat_<float> measurement(2,1); 
@@ -57,8 +57,9 @@ int main(int argc, char** argv)
     setIdentity(KF.processNoiseCov, cv::Scalar::all(1e-4));
     setIdentity(KF.measurementNoiseCov, cv::Scalar::all(10));
     setIdentity(KF.errorCovPost, cv::Scalar::all(.1));
+    // ---<< Kalman Filter Parameters ----
     
-
+    // ---Taking Camera >>-----
     cap.open(device_id,api_id);
     
     if (!cap.isOpened())            // check if succeded
@@ -68,30 +69,36 @@ int main(int argc, char** argv)
     } 
     
     std::cout<<"Start grabbing"<<'\n'<<"Press any key to terminate"<<'\n';
+    // ---<< Taking Camera ----
+
     for (;;)
     {
-        // First predict, to update the internal statePre variable
-        cv::Mat prediction = KF.predict();
-        cv::Point predictPt(prediction.at<float>(0),prediction.at<float>(1));
+        // Prediction, to update the internal statePre variable -->>
+        cv::Mat prediction  =   KF.predict();
+        cv::Point               predictPt(prediction.at<float>(0),prediction.at<float>(1));
+        //  <<---Prediction, to update the internal statePre variable
 
 
-        // Blob detector ---------
         cap.read(frame);
-        if (frame.empty())          // check if succeded
+        if  (frame.empty())          // check if succeded
         {
             std::cerr<<"ERROR! blank frame grabbed\n";
             break;
         } 
 
-        cv::cvtColor(frame, frame_HSV, cv::COLOR_BGR2HSV);
-        cv::inRange(frame_HSV, orange_min, orange_max,frame_threshold);
-        cv::findContours(frame_threshold, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
-        cv::Mat drawing = cv::Mat::zeros(frame_threshold.size(), CV_8UC3 );
-        cv::Mat drawingKF = cv::Mat::zeros(frame_threshold.size(), CV_8UC3 );
+        // Blob detector ->>------
+        cv::cvtColor            (frame, frame_HSV, cv::COLOR_BGR2HSV);
+        cv::inRange             (frame_HSV, orange_min, orange_max,frame_threshold);
+        cv::findContours        (frame_threshold, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+       
 
-        int maxAreaContourId = FindMaxAreaContourId(contours);
+        cv::Mat drawing         = cv::Mat::zeros(frame_threshold.size(), CV_8UC3 );
+        cv::Mat drawingKF       = cv::Mat::zeros(frame_threshold.size(), CV_8UC3 );
+
+        int maxAreaContourId    = FindMaxAreaContourId(contours);
         
+
         // uncomment to test FindMaxAreaContourId function
         // std::cout<<maxAreaContourId<<'\n';
 
@@ -102,39 +109,36 @@ int main(int argc, char** argv)
         {   
             
 
-            std::vector<cv::Point> contour_poly   (contours.size());
-            cv::Point2f            center         (contours.size());  
-            float                  radius          (contours.size());
+            std::vector<cv::Point>  contour_poly   (contours.size());
+            cv::Point2f             center         (contours.size());  
+            float                   radius          (contours.size());
 
-            cv::approxPolyDP(contours[maxAreaContourId], contour_poly, 3, true);
-            cv::minEnclosingCircle(contour_poly, center, radius);
+            cv::approxPolyDP        (contours[maxAreaContourId], contour_poly, 3, true);
+            cv::minEnclosingCircle  (contour_poly, center, radius);
 
             // uncomment the following to draw contours exactly
-            // cv::drawContours(drawing, contours, maxAreaContourId, detectionColor ,5, cv::LINE_8, hierarchy, 0 );
-
-            
-
+            /* cv::drawContours(drawing, contours, maxAreaContourId, 
+                                detectionColor ,5, cv::LINE_8, hierarchy, 0 );
+            */
             // uncomment the following for checking center coordinates
             // std::cout<<center<<'\n';
-            
 
-            // getting the point
+             //-<<---Blob detector
+
+            // Obtaining the point fro Kalman Filter
             measurement.at<float>(0) = center.x;
             measurement.at<float>(1) = center.y;
-            //update 
-            cv::Mat estimated = KF.correct(measurement);
-            cv::Point statePt(estimated.at<float>(0),estimated.at<float>(1));
-            cv::Point measPt(measurement(0),measurement(1));
-
-            cv::circle(drawing, statePt, int(radius), detection_color, 2 );
-            cv::circle(drawing, statePt, 5, detection_color, 10);
+            // Updating Kalman Filter
+            cv::Mat     estimated = KF.correct(measurement);
+            cv::Point   statePt(estimated.at<float>(0),estimated.at<float>(1));
+            cv::Point   measPt(measurement(0),measurement(1));
+            // Drawing Point
+            cv::circle  (drawing, statePt, int(radius), detection_color, 2 );
+            cv::circle  (drawing, statePt, 5, detection_color, 10);
 
         }
         
-
-                
         cv::namedWindow("Live", cv::WINDOW_AUTOSIZE);
-        
         cv::imshow("Live", frame + drawing);  // show image        
         
         if (cv::waitKey(5) >= 0)    // break if a key is pressed
