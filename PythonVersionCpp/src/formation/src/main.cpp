4d200c43769c4ca38e7ec4d5ea8e14de8c314ad8
uzakotim <timauzakov@gmail.com>
2021-06-23 17:16:26 +0500

    callback of formation, state and covariances

--- PythonVersionCpp/src/formation/src/main.cpp
+++ PythonVersionCpp/src/formation/src/main.cpp
@@ -16,6 +16,7 @@
 
 // include ros library
 #include <ros/ros.h>
+#include <cmath>
 
 using namespace sensor_msgs;
 using namespace message_filters;
@@ -27,27 +28,75 @@ class FormationController
 {
 private:
 
-    message_filters::Subscriber<PointStamped> sub_1;
+    message_filters::Subscriber<_Float32[]> sub_1;
     message_filters::Subscriber<Odometry> sub_2;
-    typedef sync_policies::ApproximateTime<PointStamped,Odometry> MySyncPolicy;
+    typedef sync_policies::ApproximateTime<_Float32[],Odometry> MySyncPolicy;
     typedef Synchronizer<MySyncPolicy> Sync;
     boost::shared_ptr<Sync> sync;
+
+    // parameters
+    double n_pos = 1.2;
+    double n_neg = 0.5;
+    double delta_max = 0.5;
+    double delta_min = 0.000001;
+    double radius = 6;
+
+    ros::Publisher error_pub;
+    std::string error_topic = "/uav1/error";
+    ros::Publisher pose_pub;
+    std::string pose_topic = "/uav1/control_manager/goto";
+
+    PointStamped msg;
+
+    cv::Mat tracker_vector = (cv::Mat_<float>(3,1) << 0,0,0);
+    std::vector<cv::Mat> tracker;
+    int count {0};
+    cv::Mat w_prev = (cv::Mat_<float>(3,1) <<  0,0,0);
+    cv::Mat master_pose;
+
+    // measurements
+    cv::Mat state,state_cov,human_coord,human_cov;
 
 public:
     FormationController()
     {
         ros::NodeHandle nh;
-        sub_1.subscribe(nh,"computations/goal_point",1);
+        sub_1.subscribe(nh,"/uav1/sensor_fusion",1);
         sub_2.subscribe(nh,"/odometry/odom_main",1);
     
         //  TODO: Ensure that callback is called
         sync.reset              (new Sync(MySyncPolicy(10), sub_1, sub_2));
         sync->registerCallback  (boost::bind(&FormationController::callback, this, _1, _2));
+        
+        
+        // TODO Ensure the sensor fusion message is transported and received
+        
+        error_pub = nh.advertise<_Float32[]>(error_topic, 1);
+        pose_pub = nh.advertise<PointStamped>(pose_topic, 1);
+
+        // measurements
+
+        tracker.push_back(tracker_vector);
+        tracker.push_back(tracker_vector);
         ROS_INFO("All functions initialized");
+
+
+        
     }
     void callback(const PointStampedConstPtr& goal_point, const nav_msgs::OdometryPtr &pose)
     {
+        // measurements
+        state = (cv::Mat_<float>(4,1) << pose->pose.pose.position.x,pose->pose.pose.position.y,pose->pose.pose.position.z,pose->pose.pose.orientation.z);
+        state_cov = (cv::Mat_<float>(6,6) << pose->pose.covariance); // TODO Check if recevied
+        // ROS_INFO("state covariance"<<state_cov<<'\n');
+        human_coord = (cv::Mat_<float>(3,1) << goal_point->point.x, goal_point->point.y, goal_point->point.z);
+        // human_cov = (cv::Mat_<float>(6,6) << goal_point->point.covariance); TODO 
+        tracker.push_back(human_coord);
+        count++;
+
         ROS_INFO("Synchronized\n");
+
+        // TODO Drone Safety
     }
 };
 
